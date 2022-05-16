682b37260bad52421f1128007797269a8cd1b35b
Macit Simsek <macit.simsek@ukfast.co.uk>
2022-05-16 13:55:55 +0200

    Add sales controller with view as requested UI and backend logic

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -2,17 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\ProductRepository;
+use App\Repositories\SalesRepository;
 use \Illuminate\Http\Request;
 
 class SalesController extends Controller
 {
+    private $salesRepository;
+    private $productRepository;
+    private $shipping_cost = 10;
+    
+    public function __construct(SalesRepository $salesRepository, ProductRepository $productRepository) {
+        $this->salesRepository = $salesRepository;
+        $this->productRepository = $productRepository;
+    }
     /**
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        return view('coffee_sales');
+        $sales = $this->salesRepository->all();
+        return view('coffee_sales', ['sales' => $sales]);
     }
 
     /**
@@ -20,5 +31,30 @@ class SalesController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'quantity' => 'required|numeric',
+            'unit_cost' => 'required|numeric'
+        ],[
+            'quantity.required' => 'A quantity is required',
+            'unit_cost.required' => 'A unit cost is required',
+            'quantity.numeric' => 'Quantity must be numeric',
+            'unit_cost.numeric' => 'Unit cost must be numeric',
+        ]);
+        try {
+            $product_id = $this->productRepository->getByColumn('gold_coffee','slug',['id','profit_margin']);
+            $unit_cost = $request->get('unit_cost');
+            $quantity = $request->get('quantity');
+            $cost = $unit_cost * $quantity;
+            $cost = number_format((float)$cost, 2, '.', '');
+            $selling_price = ($cost / (1-$product_id['profit_margin'])) + $this->shipping_cost;
+            $selling_price = number_format((float)$selling_price, 2, '.', '');
+            $request->merge(['selling_price' => $selling_price, 'cost' => $cost]);
+            $request->merge(['product_id' => $product_id['id']]);
+            $this->salesRepository->create($request->only(['quantity','unit_cost','selling_price','cost']));
+            return redirect()->back()->withErrors(['msg' => 'Sale successful']);
+        } catch (\Exception $e) {
+            //Log
+            return redirect()->back()->withErrors(['msg' => $e->getMessage()]);
+        }
     }
 }
--- resources/views/coffee_sales.blade.php
+++ resources/views/coffee_sales.blade.php
@@ -5,4 +5,53 @@
         </h2>
     </x-slot>
 
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                @if ($errors->any())
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <div class="col-xl-6 col-lg-6 col-md-6">
+                                <div class="alert alert-danger">
+                                    <ul>
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                        </div>
+                    </div>
+                @endif
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <form id="salesForm" method="POST" action="{{route('store.sales')}}">
+                    @csrf
+                        <div class="col-xl-6 col-lg-6 col-md-6">
+                            <input type="text" name="quantity" id="quantity" class="form-control" placeholder="Quantity" required>
+                            <input type="text" name="unit_cost" id="unit_cost" class="form-control" placeholder="Unit Cost (£)" required>
+                            <span class="p-6">Selling Price</span>
+                            <button type="submit" style="border: 1px solid black; padding:5px" class="border-gray-200">Record Sale</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <div class="col-xl-6 col-lg-6 col-md-6">
+                        <span class="pb-2">Previous Sales</span>
+                        <div>
+                            <table style="width: 60%;">
+                                <th style="border: 1px solid black">Quantity</th>
+                                <th style="border: 1px solid black">Unit Cost</th>
+                                <th style="border: 1px solid black">Selling Price</th>
+                                @foreach ($sales as $sale)
+                                <tr>
+                                    <td style="border: 1px solid black">{{$sale->quantity}}</td>
+                                    <td style="border: 1px solid black">{{$sale->unit_cost}}</td>
+                                    <td style="border: 1px solid black">{{$sale->selling_price}}</td>
+                                </tr>
+                                @endforeach
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
